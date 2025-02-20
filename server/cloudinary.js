27644734e095c00de2307b07c6c16674c7f937f9
robdeer1 <r_deer@fanshaweonline.ca>
2025-02-20 18:48:54 -0500

    updated cloudinary.js

--- server/cloudinary.js
+++ server/cloudinary.js
@@ -1,10 +1,13 @@
 const cloudinary = require("cloudinary").v2;
 
+const api_key = import.meta.env.VITE_CLOUDINARY_API_KEY;
+const api_secret = import.meta.env.VITE_CLOUDINARY_API_SECRET;
+
 // Configuration
 cloudinary.config({
   cloud_name: "dh8cvmhv0",
-  api_key: "241816583456869",
-  api_secret: "5IW1SM3oRXSw_9Qhgo_VCEUyQnc", // Click 'View API Keys' above to copy your API secret
+  api_key: api_key,
+  api_secret: api_secret, // Click 'View API Keys' above to copy your API secret
 });
 
 module.exports = cloudinary;
